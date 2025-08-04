9a0b6c2531e56624df29ff6222e70ec189898301
Don Wells <jdonwells@gmail.com>
2025-08-04 15:43:22 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -125,7 +125,7 @@ namespace robotbit {
 
     function initPCA9685(): void {
         i2cwrite(PCA9685_ADDRESS, MODE1, 0x00)
-        setFreq(49);
+        setFreq(48.5);
         for (let idx = 0; idx < 16; idx++) {
             setPwm(idx, 0, 0);
         }
@@ -611,5 +611,6 @@ namespace robotbit {
         }
     }
 }
+
 
 
